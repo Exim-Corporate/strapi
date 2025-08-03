eeee943bccae16d95afc137cc106c6fcfc9c90fc
DmytroLukianenko <DmytroLukianenko93@gmail.com>
2025-08-03 18:08:29 +0200

    Implement findOne method in article controller to retrieve articles by slug

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController('api::article.article', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params; // Получаем слаг из параметров
+    const { query } = ctx; // Получаем query из запроса
+
+    // Ищем статью по слагу
+    const entity = await strapi.entityService.findMany('api::article.article', {
+      filters: { slug },
+      ...query, // Применяем дополнительные параметры из query
+    });
+
+    if (!entity || entity.length === 0) {
+      return ctx.notFound('Article not found');
+    }
+
+    // Возвращаем первую найденную статью
+    return this.transformResponse(entity[0]);
+  },
+}));
